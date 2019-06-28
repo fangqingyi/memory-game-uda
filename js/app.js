9090bd49150ea16d5d7390857609e14775b63928
fangqingyi <719041306@qq.com>
2019-06-28 18:56:28 +0800

    shuffle cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * 创建一个包含所有卡片的数组
  */
-var pictures = ["fa-user", "fa-user", "fa-tree", "fa-tree"
+var pictures = ["fa-user", "fa-user", "fa-tree", "fa-tree",
 				"fa-television", "fa-television", "fa-bell", "fa-bell",
 				"fa-coffee", "fa-coffee", "fa-cog", "fa-cog",
 				"fa-clone", "fa-clone", "fa-heart-o", "fa-heart-o"];
@@ -12,6 +12,22 @@ var pictures = ["fa-user", "fa-user", "fa-tree", "fa-tree"
  *   - 循环遍历每张卡片，创建其 HTML
  *   - 将每张卡的 HTML 添加到页面
  */
+$(".fa-repeat").click(function(e) {
+    newPic = shuffle(pictures);        //卡片数组随机化
+    let cardsFrag = document.createDocumentFragment(); //利用Fragment构建新的卡片网格
+    for (var i = newPic.length - 1; i >= 0; i--) {
+        let cardLi = document.createElement("li");
+        cardLi.classList.add("card");//创建类为card的li元素
+        let cardFa = document.createElement("i");
+        cardFa.classList.add("fa");
+        cardFa.classList.add(newPic[i]);//创建类为fa fa-***的i元素
+        cardLi.appendChild(cardFa);
+        cardsFrag.appendChild(cardLi);//加入到Fragment中
+    }
+    $(".wangge").empty();//清空原卡片网格
+    $(".wangge").append(cardsFrag);//添加随机过的卡片网格
+    //此处还需要一个计时的起点
+});
 
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
 function shuffle(array) {
