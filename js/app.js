dd41056fbedb05419b8b2772e93acda20ae6001d
fangqingyi <719041306@qq.com>
2019-07-01 00:45:25 +0800

    delete many and start from here

--- js/app.js
+++ js/app.js
@@ -27,27 +27,7 @@ $(".fa-repeat").click(function(e) {
     }
     $(".wangge").empty();//清空原卡片网格
     $(".wangge").append(cardsFrag);//添加随机过的卡片网格
-    var isCountTime = false;//定义计时器为未打开状态
-    var countMoves = 0;//初始步数为0
-    var isGameover = false;
-    constructStars();//恢复3星
 });
-
-//恢复星级的函数  
-function constructStars() {
-    let starsFrag = document.createDocumentFragment();//利用Fragment
-    for (var i = 0; i < 3; i++) {
-        let starLi = document.createElement("li");
-        let starFa = document.createElement("i");
-        starFa.classList.add("fa");
-        starFa.classList.add("fa-star");
-        starLi.append(starFa);
-        starsFrag.append(starLi);    
-    }
-    $('stars').empty();
-    $('stars').append(starsFrag);
-    document.getElementsByClassName("moves").textContent = 3;
-}
 
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -65,38 +45,13 @@ function shuffle(array) {
 }
 
 //点击卡片后翻开，添加到openCardsArray数组
-function openCard(thisCard) {
-    thisCard.classList.add("open");
-    thisCard.classList.add("show");
-    openCardsArray.push(thisCard);
-}
+
 
 //检查两张卡片是否匹配，匹配则添加类match
-function isMatching(cardOne, cardTwo) {
-    if (cardOne.classList[1] === cardTwo.classList[1]) {//如果匹配，则保留类match
-        cardOne.removeClass("open show");
-        cardTwo.removeClass("open show");
-        cardOne.addClass("match");
-        cardTwo.addClass("match");
-        openCardsArray.pop();
-        openCardsArray.pop();
-        cardOne.off('click');                           //匹配成功的卡片去除点击事件
-        cardTwo.off('click');
-    } else {                                            //如果不匹配，则翻回去
-        cardOne.removeClass("open show");
-        cardTwo.removeClass("open show");
-        openCardsArray.pop();
-        openCardsArray.pop();
-    }
-}
+
 
 //统计所有卡片是否都已经匹配
-function isGameover() {
-    const matchCards = document.querySlectorAll(".match");
-    if (matchCards.length === 16) {
-        return true;
-    }
-}
+
 
 /*
  * 设置一张卡片的事件监听器。 如果该卡片被点击：
@@ -108,36 +63,3 @@ function isGameover() {
  *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
-$(".card").click(function(e){
-    if (!isCountTime) {                     //第一步打开计时器
-        var seconds = 0;
-        $('body').everyTime('1s', 'jishiqi', function() {
-            seconds += 1;
-        });
-        isCountTime = true;
-    }
-    isGameover();                    //判断游戏是否结束
-    if (isGameover) {               //如果游戏结束，则没有点击效果，且停止计时
-        $('body').stopTime('jishiqi');
-        afterGameover();
-        $('#myModal').modal(show);
-        return;
-    }
-    if (openCardsArray.length === 2 ) {     //判断有效点击，如果有卡片正在匹配，则不可点击
-        return;
-    }
-    countMoves += 1;              //有效点击次数+1
-    if (countMoves === 23 || countMoves === 28) {      //判断有效点击次数的值，达到23，28步时减少一个星级
-        $('.fa-star')[0].remove();
-        let movesText = document.getElementsByClassName("moves");
-        movesText.textContent = `${$('.fa-star').length}`;  //显示星星的数目
-    }
-    openCard(this);                  //调用函数翻开卡片
-    isMatching(openCardsArray[0], openCardsArray[1]);      //判断卡片是否匹配
-});
-
-function afterGameover() {               //修改模态框文本
-    const grade = document.createElement("p");
-    grade.textContent = `spend ${seconds} seconds, move ${countMoves}, get ${$('.fa-star').length} stars`;//游戏结束后，弹窗显示步数，时间，星级，
-    $('modal-body').append(grade);
-}
