31b57ce8d1f4d8d13ce239282091984bf530bb5d
fangqingyi <719041306@qq.com>
2019-07-01 03:09:52 +0800

    little change of app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ var pictures = ["fa-user", "fa-user", "fa-tree", "fa-tree",
 				"fa-coffee", "fa-coffee", "fa-cog", "fa-cog",
 				"fa-clone", "fa-clone", "fa-heart-o", "fa-heart-o"];
 var openCardsArray = [];//定义一个空的数组作为翻开的卡片数组
-
+var countMoves = 0;//初始步数为0
+var isGameOver = false;//判断游戏状态为未完成
+var isCountTime = false;//计时器状态为关闭，或者考虑new Date()?
 /*
  * 显示页面上的卡片
  *   - 使用下面提供的 "shuffle" 方法对数组中的卡片进行洗牌
@@ -14,7 +16,7 @@ var openCardsArray = [];//定义一个空的数组作为翻开的卡片数组
  *   - 将每张卡的 HTML 添加到页面
  */
 $(".fa-repeat").click(function(e) {
-    newPic = shuffle(pictures);        //卡片数组随机化
+    var newPic = shuffle(pictures);        //卡片数组随机化
     let cardsFrag = document.createDocumentFragment(); //利用Fragment构建新的卡片网格
     for (var i = newPic.length - 1; i >= 0; i--) {
         let cardLi = document.createElement("li");
@@ -63,3 +65,8 @@ function shuffle(array) {
  *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
+const cardsWangge = document.getElementsByClassName('wangge');
+cardsWangge.addEventListener('click', function(event) {         //把侦听器放在网格上，可以只放一个
+    if (event.nodeName.toLowerCase() === 'li') {                //点击类为card的元素（也就是<li>）时才启动
+    }
+});
